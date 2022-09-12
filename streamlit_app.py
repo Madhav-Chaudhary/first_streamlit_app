293dc0d38a1ad271d1970cb44b8a1a8b8e7b0b7c
Madhav-Chaudhary <64666636+Madhav-Chaudhary@users.noreply.github.com>
2022-09-12 11:45:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,4 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 streamlit.text(fruityvice_response)
+treamlit.header("Fruityvice Fruit Advice!")
